2863e6d6793c885bf6eb91eb7250c7f44d8baa46
Михаил Сычев <misha@MacBook-Pro-Mihail.local>
2021-02-01 23:30:43 +0300

    Подключил файл js и описал две функции

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomIntInclusive = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max <= min || min < 0 || max < 0) {
+    return;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getRandomFloat = function (min, max, numbers) {
+  if (max <= min || min < 0 || max < 0) {
+    return;
+  }
+  return (Math.random() * (max - min) + min).toFixed(numbers);
+}
+
+getRandomIntInclusive(0, 10);
+getRandomFloat(1.1, 1.5, 5);
+
+
+
+
