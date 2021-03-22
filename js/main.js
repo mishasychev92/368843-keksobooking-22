a2643dcc976bd174bf935a1571ebae826f5c5160
Михаил Сычев <misha@MacBook-Pro-Mihail.local>
2021-03-22 22:52:22 +0300

    Подключил lodash и реализовал рендер меток не чаще, чем раз в полсекунды

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
+/* global _:readonly */
 import { addAdPinsIntoMap } from './map.js';
 import { getData } from './api.js';
 import { showErrorPopup } from './popup.js';
 import { setFormSubmit, setMapFilterChange } from './form.js';
 
+const RENDER_DELAY = 500;
+
 getData(
   (adData) => {
     addAdPinsIntoMap(adData);
-    setMapFilterChange(() => addAdPinsIntoMap(adData));
+    setMapFilterChange(_.debounce(() => addAdPinsIntoMap(adData), RENDER_DELAY));
   },
   showErrorPopup);
 
